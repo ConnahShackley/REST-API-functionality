5ba75b10b3bf9dbbb9a8db9617a6a765d4a048d5
Username <connah.s@hotmail.com>
2022-03-18 17:39:05 +0000

    REST-API/AUTH

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 
 require("./db/connection");
 const express = require("express");
+const cors = require("cors");
 const movieRouter = require("./movie/movieRoutes");
 const userRouter = require("./user/userRoutes");
 const app = express();
-const port = 5001;
+const port = 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use(movieRouter);
 app.use(userRouter);
 
--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { addUser, login } = require("./userControllers");
-const { hashPassword, decryptPassword } = require("../middleware");
+const { hashPassword, decryptPassword, checkToken } = require("../middleware");
 const userRouter = Router();
 
 userRouter.post("/user", hashPassword, addUser);
